34bd47187d134d555b386d98dd082223b616ea50
Chance Davis <chancejd53@gmail.com>
2017-02-25 10:44:34 -0500

    Added a search function and needed keys

--- src/ajax.js
+++ src/ajax.js
@@ -1,8 +1,23 @@
-var axios = require('axios')
+var axios = require('axios');
 axios.get('https://api.github.com/users/codeheaven-io');
 
-// Lyrics
-function getSongs(){};
+// Google vars
+var googleKey = 'key=AIzaSyDKKnJzDuzPOA_0dB2PoTMWtaBn1VaP9RY';
+var googleSearch = 'cx=005367748168396004507:_mzf0ltwpxs';
+
+// Spotify vars
+var client_id = '27eb8b96e2de49f5b58e337bc2b6d638'; // Your client id
+var client_secret = 'c62ace7bd7284c8c8fe1bbda40bad08a'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+
+// Lyrics (Using google custom search)
+function getSongs(query){
+    axios.get('https://www.googleapis.com/customsearch/v1?' + googleKey + '&' + googleSearch + '&' + 'q=' + query)
+        .then(function(response){
+            console.log(response.data);
+            console.log(response.status);
+        })
+};
 
 function getSong(){};
 
